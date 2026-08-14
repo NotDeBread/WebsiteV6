b219a48f443ec16308162f7b6a971f35ca2858e4
DeBread <redbarnmo@gmail.com>
2026-08-13 22:17:36 -0500

    v7-pre13
    - Fixed image roller
    - Updated Goober Shooter 2
    - Started early work on the Goober Shooter 2 Wiki

---
+++ GSWiki/generic.js
@@ -0,0 +1,25 @@
+const index = {
+    gameplay: {
+        items: {
+            
+        },
+        bosses: {
+            
+        },
+        achievements: {
+            
+        }
+    },
+    modding: {
+
+    },
+    history: {
+
+    }
+}
+
+function renderList(obj, parent) {
+    for(key in Object.keys(obj)) {
+        const listItem = 'e'
+    }
+}
