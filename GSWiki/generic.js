a431be3f2d92ce34f384ce49b49e738b9fc32baa
DeBread <redbarnmo@gmail.com>
2026-08-24 14:12:24 -0500

    Updated Goober Shooter 2

--- GSWiki/generic.js
+++ GSWiki/generic.js
@@ -8,13 +8,14 @@ const Home = {
             Bread: {},
             Fella: {},
             Plonk: {},
+            Luke: {}
         },
         Weapons: {
             Gun: {},
             Sniper: {}
         },
         Items: {
-            Rock: {}
+            Rock: {},
         },
         Power_Items: {
             Apple: {}
@@ -26,10 +27,13 @@ const Home = {
             The_Tutorialist: {}
         },
         Minibosses: {
-            Sasha: {}
+            Sasha0Boss: {},
+            Chip0Boss: {}
         },
         Enemies: {
-            Goon: {}
+            Goon: {},
+            Chud_Chip: {},
+            Tutorialist_Servant: {}
         },
         Achievements: {},
     },
@@ -51,8 +55,8 @@ function createNavButton(obj, key, parent) {
     button.innerHTML = `
         <div class="explorerItemButtons">
             ${hasChildren ? '<button class="explorerItemArrow">►</button>' : ''}
-            <a href="#${key}">
-                <button>${key.replaceAll('_',' ')}</button>
+            <a href="#${key.replaceAll('0','')}">
+                <button>${key.replaceAll('_',' ').split('0')[0]}</button>
             </a>
         </div>
         ${hasChildren ? '<div class="explorerItemChildren"></div>' : ''}
@@ -95,7 +99,17 @@ async function loadPage(page) {
     updateCustomElements()
 }
 
+const characterUnlocks = {
+    peep: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/whoops.png">Oops</a>" achievement.',
+    the_horse: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/optimization.png">Optimization at its finest</a>" achievement.',
+    sasha: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/Speed_Demon.png">Speed Demon</a>" achievement.',
+    isaac: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/Reroll_Addict.png">Reroll Addict</a>" achievement.',
+    friend: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/The_Egg.png">The Egg</a>" achievement.',
+    tutorialist: 'Complete the "<a href="#Achievements"><img src="../../games/GooberShooter2/graphics/achievements/The_End.png">The End</a>" achievement.',
+}
+
 function updateCustomElements() {
+    //Character card
     doge('body').querySelectorAll('gsCharacter').forEach(elem => {
         const characterKey = elem.getAttribute('character')
         const character = characters[characterKey]
@@ -106,6 +120,7 @@ function updateCustomElements() {
                 <span>${character.desc}</span>
             </div>
             <img src="../../games/GooberShooter2/graphics/characters/${characterKey}Portrait.png" width="72" id="characterPortrait">
+            <div id="characterTags" style="display: flex; gap: 5px; margin-top: 5px;"></div>
             ${character.skins ? `
                 <span>Available Skins:</span>
                 <div id="characterSkins" style="display: flex; gap: 5px;"></div>    
@@ -128,6 +143,46 @@ function updateCustomElements() {
             }
 
             elem.querySelector('#characterSkins').append(img)
+        }
+
+        for(const tag of character.tagList) {
+            const div = document.createElement('div')
+            div.classList.add('characterTag')
+            div.innerHTML = tag.text
+
+            addStyles(div, {
+                background: tag.col,
+            })
+
+            doge('characterTags').append(div)
+        }
+    })
+
+    //Character list
+    doge('body').querySelectorAll('.characterTable').forEach(elem => {
+        for(const index in Object.keys(characters)) {
+            const key = Object.keys(characters)[index]
+            const character = characters[key]
+
+            const row = document.createElement('tr')
+            row.innerHTML = `
+                <td style="font-weight: 700;">
+                    <img src="../../games/GooberShooter2/graphics/characters/${key}Portrait.png" width="38" style="image-rendering: pixelated; vertical-align: middle; margin-right: 5px;">
+                    <a href="#${character.name}">${character.name}</a>
+                </td>
+                <td>
+                    ${character.desc ?? '<em style="color: grey;">No description</em>'}
+                </td>
+                <td>
+                    <img src="../../games/GooberShooter2/graphics/weapons/${character.weapon.name.replaceAll(' ','_')}.png" style="image-rendering: pixelated; vertical-align: middle; margin-right: 5px;">
+                    ${character.weapon.name}
+                </td>
+                <td style="max-width: 250px;">
+                    ${characterUnlocks[key] ?? 'N/A'}
+                </td>
+            `
+
+            elem.append(row)
         }
     })
 }
